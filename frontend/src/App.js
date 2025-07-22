8a5891b16cf970e08c9fd9133057a5da97256e53
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-23 01:47:43 +0530

    Add Footer component with social media links and integrate into App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
-
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex flex-col justify-between min-h-screen overflow-hidden
+      bg-slate-900">
           <div>
             <h1 className="text-3xl font-bold underline">
               Himanshu Sharma
               </h1>
           </div>
+          <Footer/>
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,31 @@
+import {NavLink} from "react-router";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+
+
+const Footer = () =>{
+    return(
+        <div className="flex justify-center items-center gap-8">
+            <NavLink to='https://github.com/CodeXGautam'
+            target="blank">
+                <FaGithub className="text-3xl cursor-pointer text-slate-500 hover:text-slate-300
+                transition-all duration-200"/>
+            </NavLink>
+
+            <NavLink to='https://www.linkedin.com/in/himanshu-sharma-72b93b283/' 
+            target="blank">
+                <FaLinkedin className="text-3xl cursor-pointer text-slate-500 hover:text-slate-300
+                transition-all duration-200"/>
+            </NavLink>
+
+             <NavLink to='mailto:himanshug1310@gmail.com' 
+            target="blank">
+                <BiLogoGmail className="text-3xl cursor-pointer text-slate-500 hover:text-slate-300
+                transition-all duration-200"/>
+            </NavLink>
+        </div>
+    )
+}
+
+export default Footer;
