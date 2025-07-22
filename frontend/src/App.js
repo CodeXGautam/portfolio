7d257e46a8352f0f5f578697fe6d54ba6ac336fe
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-22 18:43:36 +0530

    Refactor App component structure for improved readability

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,11 @@
 function App() {
   return (
     <div className="App">
-
+          <div>
+            <h1 className="text-3xl font-bold underline">
+              Himanshu Sharma
+              </h1>
+          </div>
     </div>
   );
 }
