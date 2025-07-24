2072ef18bee4c96beef3584f0f3b7d416756bd9a
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-25 01:10:35 +0530

    Refactor Home component to implement smooth scrolling and improve button styles

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,10 @@
-import { useNavigate } from "react-router";
 
 const Home = () => {
 
-    const navigate = useNavigate();
+    const scrollToSection = (id) => {
+        document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
+    }
+
     return (
         <div className="flex flex-col gap-5 bg-slate-950 justify-center items-center w-[100%]
         transition-all duration-300">
@@ -13,25 +15,25 @@ const Home = () => {
                  text-amber-800 font-body">
                     Himanshu Sharma
                 </h1>
-                <div className="text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-2xl font-semibold font-body">
+                <div className="text-xs sm:text-lg md:text-xl lg:text-2xl xl:text-2xl font-semibold font-body">
                     Not just writing code — building experiences.
                 </div>
 
-                <div className="flex gap-5 flex-col sm:flex-row md:flex-row lg:flex-row xl:flex-row 2xl:flex-row">
+                <div className="flex gap-5">
 
                     <div className='flex justify-center items-center p-3 rounded-tl-xl rounded-br-xl font-semibold 
-                     bg-amber-800 text-slate-200 hover:scale-[1.1] hover:text-white
-                      hover:bg-amber-700 transition-all duration-300' onClick={() => navigate('#about')}>
+                     bg-amber-800 text-slate-200 hover:scale-[1.1] hover:text-white text-xs sm:text-md md:text-lg lg:text-xl
+                      hover:bg-amber-700 transition-all duration-300' onClick={scrollToSection('about')}>
                         About
                     </div>
                     <div className='flex justify-center items-center p-3 rounded-tl-xl rounded-br-xl font-semibold
-                     bg-amber-800 text-slate-200 hover:scale-[1.1] hover:text-white
-                      hover:bg-amber-700 transition-all duration-300'>
+                     bg-amber-800 text-slate-200 hover:scale-[1.1] hover:text-white text-xs sm:text-md md:text-lg lg:text-xl
+                      hover:bg-amber-700 transition-all duration-300' onClick={scrollToSection('projects')}>
                         Projects
                     </div>
                     <div className='flex justify-center items-center p-3 rounded-tl-xl rounded-br-xl font-semibold
-                     bg-amber-800 text-slate-200 hover:scale-[1.1] hover:text-white
-                     hover:bg-amber-700 transition-all duration-300'>
+                     bg-amber-800 text-slate-200 hover:scale-[1.1] hover:text-white text-xs sm:text-md md:text-lg lg:text-xl
+                     hover:bg-amber-700 transition-all duration-300' onClick={scrollToSection('contact')}>
                         Contact
                     </div>
                 </div>
