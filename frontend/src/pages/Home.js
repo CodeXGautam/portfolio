356fee4002d32168e6e6cbdb739983f1853431a4
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-24 23:42:33 +0530

    Update styles in index.css and Home.js to enhance typography and layout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,16 +4,16 @@ const Home = () => {
 
     const navigate = useNavigate();
     return (
-        <div className="flex flex-col gap-5 bg-slate-950 relative justify-center items-center w-[100%]
+        <div className="flex flex-col gap-5 bg-slate-950 justify-center items-center w-[100%]
         transition-all duration-300">
             <img src="https://i.pinimg.com/736x/ca/a1/35/caa135a7de7ad340f167a11b3bfd3b03.jpg"
                 alt="home" className="opacity-100 w-[100%] max-h-screen" />
             <div className="absolute flex flex-col items-center gap-10 w-[70%] min-w-[150px] max-w-[980px]">
                 <h1 className="flex font-bold text-2xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-7xl
-                 text-amber-800">
+                 text-amber-800 font-body">
                     Himanshu Sharma
                 </h1>
-                <div className="text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-2xl font-semibold">
+                <div className="text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-2xl font-semibold font-body">
                     Not just writing code — building experiences.
                 </div>
 
