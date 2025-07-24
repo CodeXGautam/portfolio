c63a4512e7f6c89230cff84bf401292182df6ea1
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-25 01:46:47 +0530

    Add Projects page and integrate into App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import Footer from "./components/Footer";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <div className="flex flex-col justify-between min-h-screen overflow-hidden">
           <Home/>
           <About/>
+          <Projects/>
           <Footer/>
     </div>
   );
---
+++ frontend/src/pages/Projects.js
@@ -0,0 +1,20 @@
+
+
+const Projects = () =>{
+    return(
+         <div className="flex flex-col items-center gap-20 mt-20 justify-center min-h-screen mx-auto w-[65%] max-w-[750px]
+         min-w-[270px]" id="about">
+            <h1 className="flex flex-col justify-center items-center text-2xl text-amber-700 font-bold font-display
+            sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-5xl">
+                Projects
+                <div className="flex bg-amber-600 w-[70%] rounded-2xl h-2 -rotate-3 justify-center items-center"></div>
+            </h1>
+
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
