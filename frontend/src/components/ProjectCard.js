8f91253a9d72eafd8f12c0973e239c247a49e852
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-26 02:05:36 +0530

    Enhance ProjectCard component by updating shadow effect and ensuring links open in a new tab

--- frontend/src/components/ProjectCard.js
+++ frontend/src/components/ProjectCard.js
@@ -9,16 +9,16 @@ const ProjectCard = (props) => {
 
     return (
         <div className="flex flex-col gap-4 items-center justify-around bg-amber-100 p-5 w-[50%] max-w-[500px] min-w-[280px]
-        rounded-xl shadow-md shadow-black">
+        rounded-xl shadow-lg shadow-black">
             <h2 className='text-2xl font-bold font-body text-amber-900'>{title}</h2>
             <img src={projectImg} alt="project" className='w-[100%] object-cover' />
             <div className='font-body flex justify-center items-center'>{description}</div>
             <div className="flex justify-between items-center w-[100%]">
-                <NavLink to={gitUrl} className='flex justify-center items-center p-3 rounded-md shadow-lg bg-yellow-300 
+                <NavLink to={gitUrl} target='_blank' className='flex justify-center items-center p-3 rounded-md shadow-lg bg-yellow-300 
             hover:-translate-y-2 transition-all duration-150 hover:bg-yellow-400 font-semibold'>
                     GitHub
                 </NavLink>
-                <NavLink to={liveUrl} className='flex justify-center items-center p-3 rounded-md shadow-lg bg-yellow-300 
+                <NavLink to={liveUrl} target='_blank' className='flex justify-center items-center p-3 rounded-md shadow-lg bg-yellow-300 
             hover:-translate-y-2 transition-all duration-150 hover:bg-yellow-400 font-semibold'>
                     Demo 🚀
                 </NavLink>
