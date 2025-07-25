99ad21a9b6ee94fabbd7c4413a4b56756b0dadfd
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-26 01:45:48 +0530

    Refactor Projects page layout and update styling for improved presentation

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -2,6 +2,7 @@ import vocinteraImg from '../images/landingPage.png';
 import routewizImg from '../images/welcome_page.png';
 import mailImg from '../images/dashboard.png';
 import ProjectCard from '../components/ProjectCard';
+import { NavLink } from 'react-router';
 
 const projects = [
     {
@@ -29,7 +30,7 @@ const projects = [
 
 const Projects = () => {
     return (
-        <div className="flex flex-col items-center gap-20 mt-20 justify-center min-h-screen mx-auto w-[80%] max-w-[1080px]
+        <div className="flex flex-col items-center gap-20 mt-20 justify-center min-h-screen mx-auto w-[85%] max-w-[1180px]
          min-w-[270px]" id="about">
             <h1 className="flex flex-col justify-center items-center text-2xl text-amber-700 font-bold font-display
             sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-5xl">
@@ -37,16 +38,22 @@ const Projects = () => {
                 <div className="flex bg-amber-600 w-[70%] rounded-2xl h-2 -rotate-3 justify-center items-center"></div>
             </h1>
 
-            <div className='flex gap-4 bg-bgColor rounded-xl p-5 w-[100%] overflow-x-scroll no-scrollbar'>
+            <div className='flex gap-4 bg-bgColor rounded-xl p-10 w-[100%] overflow-x-scroll no-scrollbar'>
                 {
                     projects.map((project) => {
                         return (
-                            <ProjectCard title = {project.title} description= {project.description} 
-                            projectImg = {project.projectImg} gitUrl={project.gitUrl} liveUrl = {project.liveUrl} />
+                            <ProjectCard title={project.title} description={project.description}
+                                projectImg={project.projectImg} gitUrl={project.gitUrl} liveUrl={project.liveUrl} />
                         )
                     })
                 }
             </div>
+
+            <NavLink to="/projects_page"
+                className='flex justify-center items-center p-3 rounded-md shadow-lg bg-yellow-300 shadow-black
+            hover:-translate-y-2 transition-all duration-150 hover:bg-yellow-400 font-semibold'>
+                More →
+            </NavLink>
         </div>
     )
 }
