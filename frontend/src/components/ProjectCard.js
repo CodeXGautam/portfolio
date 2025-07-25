5ead6e9b596b3ee0c10daa9258159968e4c41fed
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-26 01:48:02 +0530

    Refactor ProjectCard component for improved layout and styling consistency

--- frontend/src/components/ProjectCard.js
+++ frontend/src/components/ProjectCard.js
@@ -1,21 +1,27 @@
-import {NavLink} from 'react-router';
+import { NavLink } from 'react-router';
 
-const ProjectCard = (props) =>{
+const ProjectCard = (props) => {
     const title = props.title;
     const projectImg = props.projectImg;
     const description = props.description;
     const gitUrl = props.gitUrl;
     const liveUrl = props.liveUrl;
 
-    return(
-        <div className="flex flex-col gap-4 items-center justify-around bg-amber-100 p-5 w-[40%] max-w-[450px] min-w-[270px]">
-            <h2 className='text-2xl font-bold font-body'>{title}</h2>
-            <img src={projectImg}  alt="project" className='w-[100%] object-cover'/>
+    return (
+        <div className="flex flex-col gap-4 items-center justify-around bg-amber-100 p-5 w-[50%] max-w-[500px] min-w-[280px]
+        rounded-xl shadow-md shadow-black">
+            <h2 className='text-2xl font-bold font-body text-amber-900'>{title}</h2>
+            <img src={projectImg} alt="project" className='w-[100%] object-cover' />
             <div className='font-body flex justify-center items-center'>{description}</div>
-            <button className='bg-yellow-200 hover:bg-yellow-300 p-5 rounded-full cursor-pointer'>Know More</button>
             <div className="flex justify-between items-center w-[100%]">
-                <NavLink to={gitUrl} className='flex justify-center items-center p-4 bg-yellow-200'>GitHub</NavLink>
-                <NavLink to={liveUrl} className='flex justify-center items-center p-4 bg-yellow-200'>Live Demo</NavLink>
+                <NavLink to={gitUrl} className='flex justify-center items-center p-3 rounded-md shadow-lg bg-yellow-300 
+            hover:-translate-y-2 transition-all duration-150 hover:bg-yellow-400 font-semibold'>
+                    GitHub
+                </NavLink>
+                <NavLink to={liveUrl} className='flex justify-center items-center p-3 rounded-md shadow-lg bg-yellow-300 
+            hover:-translate-y-2 transition-all duration-150 hover:bg-yellow-400 font-semibold'>
+                    Demo 🚀
+                </NavLink>
             </div>
         </div>
     )
