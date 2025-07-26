48cb98d302c85b6b959c982987b08d41eccca4bc
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-26 23:02:22 +0530

    Fix indentation in Contact component for improved readability

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -2,7 +2,7 @@
 
 const Contact = () =>{
     return(
-             <div className="flex flex-col items-center gap-20 mt-20 justify-center min-h-screen mx-auto w-[65%] max-w-[750px]
+            <div className="flex flex-col items-center gap-20 mt-20 justify-center min-h-screen mx-auto w-[65%] max-w-[750px]
          min-w-[270px]" id="about">
             <h1 className="flex flex-col justify-center items-center text-2xl text-amber-700 font-bold font-display
             sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-5xl">
