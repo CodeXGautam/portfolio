18ab6e2f2fa279907c723bf41b2c80264d48de88
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-28 23:15:45 +0530

    updated the ui in at some places in css

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -3,7 +3,7 @@
 const Contact = () =>{
     return(
             <div className="flex flex-col items-center gap-20 mt-20 justify-center min-h-screen
-             mx-auto w-[65%] max-w-[750px] min-w-[270px] relative overflow-hidden contact" id="about">
+             mx-auto w-[65%] max-w-[750px] min-w-[270px] relative overflow-hidden" id="contact">
             <h1 className="flex flex-col justify-center items-center text-2xl text-amber-700 font-bold font-display sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-5xl drop-shadow-lg">
                 Get In Touch !
                 <div className="flex bg-amber-600 w-[70%] rounded-2xl h-2 -rotate-3 justify-center items-center"></div>
