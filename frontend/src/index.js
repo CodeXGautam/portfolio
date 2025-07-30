9836f9878571ab9e0d2745477b3fb06911e8ebe0
Himanshu Sharma <himanshug1310@gmail.com>
2025-07-30 21:02:12 +0530

    refactor: remove unused AOS imports from index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
